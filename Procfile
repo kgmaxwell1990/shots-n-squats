b016342288ba1aacbbbda8fed59b234b865e1b6a
Katie Maxwell <katie@codeinstitute.net>
2018-03-12 21:47:09 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shots_n_squats.wsgi:application
---
+++ shots_n_squats/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shots_n_squats project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shots_n_squats.settings")
+
+application = get_wsgi_application()
